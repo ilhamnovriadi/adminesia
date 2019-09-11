b11ecef4509623411d01b5f6295a767155ac1098
cauza <cauza04@gmail.com>
2019-09-12 05:52:25 +0700

    fix bug script js and css

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -1,7 +1,24 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-12 mt-5">
+    <div>
+        <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Data User</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">User</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+        <div class="col-12">
             <div class="card">
               <div class="card-header">
                 <h3 class="card-title">Manajemen Akun</h3>
@@ -41,6 +58,8 @@
             <!-- /.card -->
           </div>
         </div>
+        </div>
+        </section>
         <div class="modal fade" id="addNew">
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
